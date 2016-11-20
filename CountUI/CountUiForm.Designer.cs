d76d3f125683e506e36cfb8e03d4d0ac4c02725e
cedarlakeinstruments <lyndon@cedarlakeinstruments.com>
2016-11-20 10:33:31 -0600

    Initial commit

---
+++ CountUI/CountUiForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Tick
+{
+    partial class CountUiForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBoxPorts = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelCount
+            // 
+            this.labelCount.BackColor = System.Drawing.Color.Black;
+            this.labelCount.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCount.ForeColor = System.Drawing.Color.Gold;
+            this.labelCount.Location = new System.Drawing.Point(7, 22);
+            this.labelCount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(211, 75);
+            this.labelCount.TabIndex = 0;
+            this.labelCount.Text = "0";
+            this.labelCount.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(7, 127);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 28);
+            this.buttonReset.TabIndex = 1;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBoxPorts);
+            this.groupBox1.Controls.Add(this.labelCount);
+            this.groupBox1.Controls.Add(this.buttonReset);
+            this.groupBox1.Location = new System.Drawing.Point(24, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(270, 186);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Count";
+            // 
+            // listBoxPorts
+            // 
+            this.listBoxPorts.FormattingEnabled = true;
+            this.listBoxPorts.ItemHeight = 20;
+            this.listBoxPorts.Location = new System.Drawing.Point(170, 131);
+            this.listBoxPorts.Name = "listBoxPorts";
+            this.listBoxPorts.Size = new System.Drawing.Size(84, 24);
+            this.listBoxPorts.TabIndex = 12;
+            this.listBoxPorts.SelectedIndexChanged += new System.EventHandler(this.portSelected);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelStatus.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.ForeColor = System.Drawing.Color.Blue;
+            this.labelStatus.Location = new System.Drawing.Point(0, 236);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(315, 20);
+            this.labelStatus.TabIndex = 11;
+            // 
+            // CountUiForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 256);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "CountUiForm";
+            this.Text = "Counter Display";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.ListBox listBoxPorts;
+    }
+}
+
---
+++ CountUI/CountUiForm.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+using CountUI.Properties;
+
+namespace Tick
+{
+    public partial class CountUiForm : Form
+    {
+        bool _connected;
+        SerialPort _port;
+
+        public CountUiForm()
+        {
+            InitializeComponent();
+            string[] names = SerialPort.GetPortNames();
+            if (names.Length  != 0)
+            {
+                foreach (string name in names)
+                {
+                    listBoxPorts.Items.Add(name);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No COM ports found");                
+            }
+
+            if (Settings.Default.port != "")
+            {
+                _connected = connect(Settings.Default.port);
+            }
+            else
+            {
+                this.labelStatus.Text = "Not connected";
+            }
+        }
+
+        bool connect(string portName)
+        {
+            timer1.Enabled = false;
+
+            if (_port != null && _port.IsOpen)
+            {
+                _port.Close();
+            }
+
+            bool connected = false;
+            try
+            {
+                _port = new SerialPort(portName, 19200);
+                _port.Handshake = Handshake.None;
+                _port.DataBits = 8;
+                _port.StopBits = StopBits.One;
+                _port.Parity = Parity.None;
+                _port.Encoding = new ASCIIEncoding();
+                _port.Open();
+                connected = true;
+            }
+            catch (IOException e)
+            {
+                this.labelStatus.Text = String.Format("Could not open {0}. {1}",portName, e.Message);
+            }
+
+            if (connected)
+            {
+                this.labelStatus.Text = String.Format("Connected to {0}", portName);
+            }
+
+            return connected;
+        }
+
+        void update()
+        {
+            this._port.WriteLine("c");
+            Thread.Sleep(50);
+            String returned = this._port.ReadExisting();
+            this.labelCount.Text = returned;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (_connected == true)
+            {
+                update();
+            }
+            else
+            {
+                this.labelStatus.Text = "Not connected";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Called when a COM port is chosen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void portSelected(object sender, EventArgs e)
+        {
+            Settings.Default.port = (string)((ListBox)sender).SelectedItem;
+            Settings.Default.Save();
+            connect(Settings.Default.port);
+        }
+    }
+}
